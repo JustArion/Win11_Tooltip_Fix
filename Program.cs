d5aa4509f8bb7ed1b2b342022a7edeb2772dce3a
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-10-28 19:28:03 +0200

    Bugfix: Right Click Context Menu gets Clicked through

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 namespace PopupHost_ClickThroughPatch;
 
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using PInvoke;
 
@@ -54,18 +55,45 @@ static class Program
         catch {} // Some GC racing conditions can cause this to fail
     }
 
-    private static bool SetTransparent(IntPtr windowHandle)
+    private static unsafe bool SetTransparent(IntPtr windowHandle)
     {
         var style = (User32.SetWindowLongFlags) User32.GetWindowLong(windowHandle, User32.WindowLongIndexFlags.GWL_EXSTYLE);
         if (style.HasFlag(User32.SetWindowLongFlags.WS_EX_TRANSPARENT) &&
             style.HasFlag(User32.SetWindowLongFlags.WS_EX_LAYERED)) return true;
+
+        var size = default(RECT);
+        try
+        {
+            size = GetWindowSize(windowHandle);
+            var height = size.bottom - size.top;
+            if (height != 41) return false; // 41 is the absolute height of the popup
+        }
+        catch 
+        { 
+            return false; // The window was closed. The element is no longer valid. (Very likely the user moved past the element.
+        }
         
         style |= User32.SetWindowLongFlags.WS_EX_TRANSPARENT | User32.SetWindowLongFlags.WS_EX_LAYERED;
         var retVal = User32.SetWindowLong(windowHandle, User32.WindowLongIndexFlags.GWL_EXSTYLE, style);
-
+        
         if (retVal != 0)
         {
-            Console.WriteLine($"Window at '{windowHandle}' Set Transparent!");
+            // Process process = null;
+            // try
+            // {
+            //     // Get the PID
+            //     User32.GetWindowThreadProcessId(windowHandle, out var pid);
+            //     // Get the process
+            //     process = Process.GetProcessById(pid);
+            // }
+            // catch {}
+            
+            // 1024x768: Width: 252, Height: 41
+            // 1920x1080: Width: 252, Height: 41
+            // The Popup has an absolute width. So we can look for that.
+            // var width = size.right - size.left;
+            // var height = size.bottom - size.top;
+            // Console.WriteLine($"Window at '{process?.ProcessName}' - {width}x{height}");
             return true;
         }
         
@@ -73,28 +101,16 @@ static class Program
         Console.WriteLine("Error setting window style: {0}", new Win32Exception(error));
         return false;
     }
+    
+    private static bool _IsEmpty(RECT rect) => rect.left == 0 && rect.top == 0 && rect.right == 0 && rect.bottom == 0;
 
-    private static async Task InitializePollingLoop()
+    private static RECT GetWindowSize(IntPtr windowHandle)
     {
-        while (true)
-        {
-            await Task.Delay(100);
-
-            var hWnd = User32.FindWindow("Xaml_WindowedPopupClass", null);
-            
-            if (hWnd == IntPtr.Zero) continue;
-
-            Console.WriteLine("Found a window at {0}", hWnd);
-            
-            var style = User32.GetWindowLong(hWnd, User32.WindowLongIndexFlags.GWL_EXSTYLE);
-            style |= (int)User32.SetWindowLongFlags.WS_EX_TRANSPARENT | (int)User32.SetWindowLongFlags.WS_EX_LAYERED;
-            var retVal = User32.SetWindowLong(hWnd, User32.WindowLongIndexFlags.GWL_EXSTYLE, (User32.SetWindowLongFlags)style);
-            if (retVal == 0)
-            {
-                var error = Marshal.GetLastWin32Error();
-                Console.WriteLine("Error setting window style: {0}", new Win32Exception(error));
-            }
-
-        }
+        Thread.Sleep(2); // Wait to Initialize
+        User32.GetWindowRect(windowHandle, out var rect);
+        if (_IsEmpty(rect)) 
+            User32.GetClientRect(windowHandle, out rect);
+        return rect;
     }
+
 }
