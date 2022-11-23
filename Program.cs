9e188f2dd1fee3e2b3efce334fb0b710f0591af4
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-11-23 12:40:36 +0100

    Explorer Context Menu Fix

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 namespace PopupHost_ClickThroughPatch;
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Microsoft.VisualBasic.Logging;
+using Newtonsoft.Json;
 using PInvoke;
 
 static class Program
@@ -23,94 +26,113 @@ static class Program
     private static void InitializeConsole()
     {
         var attached = Kernel32.AttachConsole(-1);
+
+        AppDomain.CurrentDomain.UnhandledException += (_, eo) => Trace.TraceError((eo.ExceptionObject as Exception)?.ToString());
         
+
+        Trace.Listeners.Add(new FileLogTraceListener 
+        { 
+            Name = "PopupHost.log", 
+            Location = LogFileLocation.ExecutableDirectory // For some reason this is now not the default?
+        });
+        Trace.AutoFlush = true;
+
         if (!attached) return;
         
         var stdOut = new StreamWriter(Console.OpenStandardOutput());
         stdOut.AutoFlush = true;
         Console.SetOut(stdOut);
-        
-        Console.WriteLine("Attached Console Session");
+        Trace.Listeners.Add(new ConsoleTraceListener());
+
+        Trace.WriteLine("Attached Console Session");
     }
 
     private static void InitializeWinEventHook()
     {
         var hHandle = User32.SetWinEventHook(User32.WindowsEventHookType.EVENT_OBJECT_SHOW,
-            User32.WindowsEventHookType.EVENT_OBJECT_SHOW, IntPtr.Zero, WinHookCallback, 0, 0,
+            User32.WindowsEventHookType.EVENT_OBJECT_SHOW, 
+            IntPtr.Zero, 
+            WinHookCallback, 0, 0,
             User32.WindowsEventHookFlags.WINEVENT_OUTOFCONTEXT);
 
         if (hHandle.DangerousGetHandle() != IntPtr.Zero) return;
-        MessageBox.Show($"Program Failed with error code '{Marshal.GetLastWin32Error()}");
+        MessageBox.Show($"Program Failed with error code '{Marshal.GetLastWin32Error()}'", Application.ProductName);
         Application.Exit();
     }
 
+    #if DEBUG
+    private const int _DebugProcessID = 9900;
+    #endif
     private static void WinHookCallback(IntPtr hookHandle, User32.WindowsEventHookType @event, IntPtr hwnd, int idobject, int idchild, int dweventthread, uint dwmseventtime)
     {
         try
         {
             if (User32.GetClassName(hwnd) != "Xaml_WindowedPopupClass") return;
 
+            // if (ProcessIDFromWindowHandle(hwnd) != 9900) return;
+
+            var owner = User32.GetWindow(hwnd, User32.GetWindowCommands.GW_OWNER);
+            var ownerClassName = User32.GetClassName(owner);
+            
+            /// This is the 'Windows Explorer' right click context menu.
+            /// It is treated as a Xaml Popup.
+            if (ownerClassName is "CabinetWClass")
+                return;
+
+            #if DEBUG
+            var info = new Dictionary<string, object>()
+            {
+                {"Process", ProcessNameFromWindowHandle(hwnd)},
+                {"ClassName", User32.GetClassName(hwnd)},
+                {"Text", User32.GetWindowText(hwnd) },
+                {"Owner", new Dictionary<string, object>
+                {
+                    {"ClassName", ownerClassName},
+                    {"Text", User32.GetWindowText(owner)}
+                }}
+                
+            };
+            Trace.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
+            #endif
+
             SetTransparent(hwnd);
         }
-        catch {} // Some GC racing conditions can cause this to fail
+        catch {} // If the Handle is disposed while we work with it, we don't care.
     }
 
-    private static unsafe bool SetTransparent(IntPtr windowHandle)
+    private static void SetTransparent(IntPtr windowHandle)
     {
         var style = (User32.SetWindowLongFlags) User32.GetWindowLong(windowHandle, User32.WindowLongIndexFlags.GWL_EXSTYLE);
-        if (style.HasFlag(User32.SetWindowLongFlags.WS_EX_TRANSPARENT) &&
-            style.HasFlag(User32.SetWindowLongFlags.WS_EX_LAYERED)) return true;
+        if (HasTransparentFlags(style)) return;
 
-        var size = default(RECT);
-        try
-        {
-            size = GetWindowSize(windowHandle);
-            var height = size.bottom - size.top;
-            if (height != 41) return false; // 41 is the absolute height of the popup
-        }
-        catch 
-        { 
-            return false; // The window was closed. The element is no longer valid. (Very likely the user moved past the element.
-        }
-        
-        style |= User32.SetWindowLongFlags.WS_EX_TRANSPARENT | User32.SetWindowLongFlags.WS_EX_LAYERED;
+        SetTransparentFlags(ref style);
         var retVal = User32.SetWindowLong(windowHandle, User32.WindowLongIndexFlags.GWL_EXSTYLE, style);
         
-        if (retVal != 0)
-        {
-            // Process process = null;
-            // try
-            // {
-            //     // Get the PID
-            //     User32.GetWindowThreadProcessId(windowHandle, out var pid);
-            //     // Get the process
-            //     process = Process.GetProcessById(pid);
-            // }
-            // catch {}
-            
-            // 1024x768: Width: 252, Height: 41
-            // 1920x1080: Width: 252, Height: 41
-            // The Popup has an absolute width. So we can look for that.
-            // var width = size.right - size.left;
-            // var height = size.bottom - size.top;
-            // Console.WriteLine($"Window at '{process?.ProcessName}' - {width}x{height}");
-            return true;
-        }
-        
-        var error = Marshal.GetLastWin32Error();
-        Console.WriteLine("Error setting window style: {0}", new Win32Exception(error));
-        return false;
+        if (retVal != 0) return;
+
+        Trace.TraceError($"Error setting window style: {new Win32Exception(Marshal.GetLastWin32Error())}");
     }
     
-    private static bool _IsEmpty(RECT rect) => rect.left == 0 && rect.top == 0 && rect.right == 0 && rect.bottom == 0;
-
-    private static RECT GetWindowSize(IntPtr windowHandle)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool HasTransparentFlags(User32.SetWindowLongFlags style) => style.HasFlag(User32.SetWindowLongFlags.WS_EX_TRANSPARENT) && style.HasFlag(User32.SetWindowLongFlags.WS_EX_LAYERED);
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private static void SetTransparentFlags(ref User32.SetWindowLongFlags style) => style |= User32.SetWindowLongFlags.WS_EX_TRANSPARENT | User32.SetWindowLongFlags.WS_EX_LAYERED;
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static string ProcessNameFromWindowHandle(IntPtr hwnd, bool pidOnFail = true)
     {
-        Thread.Sleep(2); // Wait to Initialize
-        User32.GetWindowRect(windowHandle, out var rect);
-        if (_IsEmpty(rect)) 
-            User32.GetClientRect(windowHandle, out rect);
-        return rect;
+        _ = User32.GetWindowThreadProcessId(hwnd, out var pid);
+        try
+        {
+            return Process.GetProcessById(pid).ProcessName;
+        }
+        catch
+        {
+            if (pidOnFail) 
+                return pid.ToString();
+            throw;
+        }
     }
 
 }
