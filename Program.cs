8d9e4e1315cf8941b164c03774882e0c5d9e37f0
arion <53127594+Arion-Kun@users.noreply.github.com>
2022-11-27 11:00:46 +0100

    Bugfix: Explorer Window Tooltips now work!
    Using the Interop.UIAutomationClient Package, We are now able to get tons more information from Windows Explorer, so tooltips are easier to find and Set them transparent.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ namespace PopupHost_ClickThroughPatch;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Newtonsoft.Json;
+using Interop.UIAutomationClient;
 using PInvoke;
 
 static class Program
@@ -40,7 +40,7 @@ static class Program
     }
 
     #if DEBUG
-    private const int _DebugProcessID = 9900;
+    private const int _DebugProcessID = 0;
     #endif
     private static void InitializeWinEventHook()
     {
@@ -73,30 +73,37 @@ static class Program
             
             /// This is the 'Windows Explorer' right click context menu.
             /// It is treated as a Xaml Popup.
-            if (ownerClassName is "CabinetWClass")
+            if (ownerClassName is "CabinetWClass" && !IsValidExplorerWindow(hwnd))
                 return;
-
-            #if DEBUG
-            var info = new Dictionary<string, object>()
-            {
-                {"Process", ProcessNameFromWindowHandle(hwnd)},
-                {"ClassName", User32.GetClassName(hwnd)},
-                {"Text", User32.GetWindowText(hwnd) },
-                {"Owner", new Dictionary<string, object>
-                {
-                    {"ClassName", ownerClassName},
-                    {"Text", User32.GetWindowText(owner)}
-                }}
-                
-            };
-            Trace.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
-            #endif
 
             SetTransparent(hwnd);
         }
-        catch {} // If the Handle is disposed while we work with it, we don't care.
+        // If the Handle is disposed while we work with it, we don't care.
+        catch (Exception e) when (e is Win32Exception) { }
     }
+    
+    private static readonly CUIAutomationClass _Automation = new();
+    private static bool IsValidExplorerWindow(IntPtr hwnd)
+    {
+        try
+        {
+            if (hwnd == IntPtr.Zero) return false;
+            var element = _Automation.ElementFromHandle(hwnd);
+            if (element == null) return false;
 
+            var popup = element.FindFirst(TreeScope.TreeScope_Children, _Automation.ControlViewCondition);
+
+            var child = popup.FindFirst(TreeScope.TreeScope_Children, _Automation.ControlViewCondition);
+
+            #if DEBUG
+            Console.WriteLine($"Type: '{child.CurrentClassName}' - '{child.CurrentName}'");
+            #endif
+            return child.CurrentClassName == "ToolTip";
+        }
+        // The user moved over a tooltip so fast that it was disposed before we could get to it.
+        catch (NullReferenceException) { return false; }
+    }
+    
     private static void SetTransparent(IntPtr windowHandle)
     {
         var style = (User32.SetWindowLongFlags) User32.GetWindowLong(windowHandle, User32.WindowLongIndexFlags.GWL_EXSTYLE);
@@ -115,23 +122,4 @@ static class Program
     
     [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     private static void SetTransparentFlags(ref User32.SetWindowLongFlags style) => style |= User32.SetWindowLongFlags.WS_EX_TRANSPARENT | User32.SetWindowLongFlags.WS_EX_LAYERED;
-    
-    #if DEBUG
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static string ProcessNameFromWindowHandle(IntPtr hwnd, bool pidOnFail = true)
-    {
-        _ = User32.GetWindowThreadProcessId(hwnd, out var pid);
-        try
-        {
-            return Process.GetProcessById(pid).ProcessName;
-        }
-        catch
-        {
-            if (pidOnFail) 
-                return pid.ToString();
-            throw;
-        }
-    }
-    #endif
-
 }
