a3f6e96df47153ea0ba2189ef2e70e6d9ebf8d8c
arion <53127594+JustArion@users.noreply.github.com>
2023-01-26 05:12:27 +0100

    Build Steps Update

--- Build.ps1
+++ Build.ps1
@@ -1,3 +1,3 @@
 dotnet restore ./src/;
 dotnet publish ./src/ --no-restore --runtime win-x86 -p:PublishSingleFile=true --output ./src/Publish --no-self-contained --configuration Release;
-echo "Built Files to './Publish'";
+echo "Built Files to './src/Publish'";
