6507e2ba99ddb18a8d4f793b46caa06afc3f96f5
arion <53127594+JustArion@users.noreply.github.com>
2023-08-26 16:41:23 +0200

    ASP Installers Removal

--- src/Start_Tooltip_Fix/Helpers/Errors.cs
+++ src/Start_Tooltip_Fix/Helpers/Errors.cs
@@ -1,19 +1,7 @@
 ﻿namespace Start_Tooltip_Fix;
 
-using Implementations;
-
 public static class Errors
 {
-    public const int UNAUTHORIZED = 5;
-    public const int DOES_NOT_EXIST = 1060;
-    public const int SUCCESS = 0;
-
-    public static void ThrowUnauthorizedError(string serviceName)
-    {
-        MessageBox.Show("You need to run this program as Administrator", serviceName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-        Environment.Exit(UNAUTHORIZED);
-    }
-
     public static void Panic(Exception e)
     {
         MessageBox.Show($"An Unexpected error occurred: {e.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
--- src/Start_Tooltip_Fix/Helpers/Strings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Start_Tooltip_Fix;
-
-public static class Strings
-{
-    private const string QUOTE = "\"{0}\"";
-    public static string SurroundQuotes(string s) => string.Format(QUOTE, s);
-}
